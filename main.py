c5414eb64b18f88b17323f92be86ef7bef165d84
Mikhail Druzhinin <druzhininmy@gmail.com>
2020-06-17 14:44:00 +0300

    create_user added

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
+from fastapi import Response
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id_: int = None
+    username: str
 
 app = FastAPI()
 
 fake_db = [
-    {'id': 0, 'username': 'Alice'},
-    {'id': 1, 'username': 'Bob'},
-    {'id': 2, 'username': 'Carol'},
+    {'id_': 0, 'username': 'Alice'},
+    {'id_': 1, 'username': 'Bob'},
+    {'id_': 2, 'username': 'Carol'},
 ]
 
 
-@app.post('/user/')
-def create_user():
+@app.post('/user/', status_code=201)
+def create_user(user: User, response: Response):
     '''Create a new user'''
-    pass
+    user = user.dict()
+    user['id_'] = fake_db[-1]['id_'] + 1
+    fake_db.append(user)
+    location = f"/user/{user['id_']}"
+    response.headers['Location'] = location
 
 
 @app.get('/users/')
@@ -25,11 +36,10 @@ def get_users():
 @app.get('/user/{user_id}')
 def get_user(user_id: int):
     '''Get a specific user'''
-    user = list(filter(lambda user: user['id'] == user_id, fake_db))
+    user = list(filter(lambda user: user['id_'] == user_id, fake_db))
     if user:
         return user[0]
     raise HTTPException(status_code=404, detail='User not found')
-    
 
 
 @app.put('/user/{user_id}')
@@ -41,9 +51,8 @@ def edit_user():
 @app.delete('/user/{user_id}', status_code=204)
 def delete_user(user_id: int):
     '''Delete user'''
-    user = list(filter(lambda user: user['id'] == user_id, fake_db))
+    user = list(filter(lambda user: user['id_'] == user_id, fake_db))
     if user:
         fake_db.remove(user[0])
     else:
         raise HTTPException(status_code=404, detail='User not found')
-    
